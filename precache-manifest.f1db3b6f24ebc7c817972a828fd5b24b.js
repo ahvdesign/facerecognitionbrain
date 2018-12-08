fe580297cc0a7955f422184d2670a11252fd6de5
Alexander Henriquez <ahvdesign@Alexanders-iMac.fios-router.home>
2018-12-08 15:05:52 -0500

    Updates

---
+++ precache-manifest.f1db3b6f24ebc7c817972a828fd5b24b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "688fbafd2f45840ece16",
+    "url": "/facerecognitionbrain/static/css/main.720af14a.chunk.css"
+  },
+  {
+    "revision": "688fbafd2f45840ece16",
+    "url": "/facerecognitionbrain/static/js/main.688fbafd.chunk.js"
+  },
+  {
+    "revision": "dbbfb3a01117253ee8b7",
+    "url": "/facerecognitionbrain/static/css/1.a35c6c98.chunk.css"
+  },
+  {
+    "revision": "dbbfb3a01117253ee8b7",
+    "url": "/facerecognitionbrain/static/js/1.dbbfb3a0.chunk.js"
+  },
+  {
+    "revision": "002c4e9bd45a266a78e3",
+    "url": "/facerecognitionbrain/static/js/runtime~main.002c4e9b.js"
+  },
+  {
+    "revision": "5eef20db697db4c73584c2bd36f1dd26",
+    "url": "/facerecognitionbrain/index.html"
+  }
+];
