e463e50aebd380020efb5c6b55289b986cca5493
Alexander Henriquez <ahvdesign@Alexanders-iMac.fios-router.home>
2018-12-08 15:05:21 -0500

    Created base components

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Rank.css';
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">{'Alex, your current rank is...'}</div>
+      <div className="white f1">{'#1'}</div>
+    </div>
+  );
+};
+
+export default Rank;
